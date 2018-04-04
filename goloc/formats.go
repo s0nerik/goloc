faa716ad2deb6e2b3444ccf60e6ed4a4396852ca
Alex Isaienko <thesonerik@gmail.com>
2018-04-04 03:17:53 +0300

    formats.go: improved errors reporting.

--- goloc/formats.go
+++ goloc/formats.go
@@ -1,27 +1,27 @@
 package goloc
 
 import (
-	"errors"
 	"fmt"
 	"strings"
+	"github.com/s0nerik/goloc/utils"
 )
 
 func ParseFormats(
 	rawData [][]interface{},
 	platform Platform,
 	formatsTabName string,
-	formatNameColumn string,
+	formatColumnTitle string,
 ) (Formats, error) {
 	firstRow := rawData[0]
 	if firstRow == nil {
-		return nil, errors.New(fmt.Sprintf(`there's no first row in the "%v" tab`, formatsTabName))
+		return nil, &noFirstRowError{tab: formatsTabName}
 	}
 
 	var formatColIndex = -1
 	var platformColIndex = -1
 	var actualPlatformName = ``
 	for i, val := range firstRow {
-		if val == formatNameColumn {
+		if val == formatColumnTitle {
 			formatColIndex = i
 		}
 		for _, name := range platform.Names() {
@@ -33,55 +33,117 @@ func ParseFormats(
 	}
 
 	if formatColIndex == -1 {
-		return nil, errors.New(fmt.Sprintf(`"%v" column not found in the first row of "%v" tab`, formatNameColumn, formatsTabName))
+		return nil, &noFormatColumnError{tab: formatsTabName, requiredColumnTitle: formatColumnTitle}
 	}
 
 	if platformColIndex == -1 {
-		return nil, errors.New(fmt.Sprintf(`can't find any of %v columns in the first row of "%v" tab`, platform.Names(), formatsTabName))
+		return nil, &noPlatformColumnError{tab: formatsTabName, platformNames: platform.Names()}
 	}
 
 	formats := Formats{}
 	for rowIndex, row := range rawData[1:] {
 		actualRowIndex := uint(rowIndex + 2)
 		if formatColIndex >= len(row) {
-			return nil, &formatNameNotSpecifiedError{tab: formatsTabName, row: actualRowIndex}
+			return nil, &formatKeyNotSpecifiedError{
+				cell: cell{tab: formatsTabName, row: actualRowIndex, column: uint(formatColIndex)},
+			}
 		}
 		if platformColIndex >= len(row) {
-			return nil, &formatValueNotSpecifiedError{tab: formatsTabName, row: actualRowIndex, platformName: actualPlatformName}
+			return nil, &formatValueNotSpecifiedError{
+				cell: cell{tab: formatsTabName, row: actualRowIndex, column: uint(platformColIndex)},
+				platformName: actualPlatformName,
+			}
 		}
 		if key, ok := row[formatColIndex].(FormatKey); ok {
 			if val, ok := row[platformColIndex].(string); ok {
 				trimmedVal := strings.TrimSpace(val)
 				if len(trimmedVal) == 0 {
-					return nil, &formatValueNotSpecifiedError{tab: formatsTabName, row: actualRowIndex, platformName: actualPlatformName}
+					return nil, &formatValueNotSpecifiedError{
+						cell:         cell{tab: formatsTabName, row: actualRowIndex, column: uint(platformColIndex)},
+						platformName: actualPlatformName,
+					}
 				}
 				formats[key] = trimmedVal
 			} else {
-				return nil, errors.New(fmt.Sprintf(`%v!%v: wrong value type`, formatsTabName, actualRowIndex))
+				return nil, &wrongValueTypeError{
+					cell: cell{tab: formatsTabName, row: actualRowIndex, column: uint(platformColIndex)},
+				}
 			}
 		} else {
-			return nil, errors.New(fmt.Sprintf(`%v!%v: wrong key type`, formatsTabName, actualRowIndex))
+			return nil, &wrongKeyTypeError{
+				cell: cell{tab: formatsTabName, row: actualRowIndex, column: uint(formatColIndex)},
+			}
 		}
 	}
 
 	return formats, nil
 }
 
-type formatNameNotSpecifiedError struct {
+type noFirstRowError struct {
 	tab string
-	row uint
 }
 
-func (e *formatNameNotSpecifiedError) Error() string {
-	return fmt.Sprintf(`%v!%v: format name is not specified`, e.tab, e.row)
+type noFormatColumnError struct {
+	tab                 string
+	requiredColumnTitle string
+}
+
+type noPlatformColumnError struct {
+	tab           string
+	platformNames []string
+}
+
+type cell struct {
+	tab    string
+	row    uint
+	column uint
+}
+
+type formatKeyNotSpecifiedError struct {
+	cell cell
 }
 
 type formatValueNotSpecifiedError struct {
-	tab          string
-	row          uint
 	platformName string
+	cell         cell
+}
+
+type wrongValueTypeError struct {
+	cell cell
+}
+
+type wrongKeyTypeError struct {
+	cell cell
+}
+
+func (c cell) String() string {
+	return fmt.Sprintf(`%v!%v%v`, c.tab, utils.ColumnName(c.column), c.row)
+}
+
+func (e *noFirstRowError) Error() string {
+	return fmt.Sprintf(`%v!A1: first row is required`, e.tab)
+}
+
+func (e *noFormatColumnError) Error() string {
+	return fmt.Sprintf(`%v!A1: "%v" column is missing in the first row`, e.tab, e.requiredColumnTitle)
+}
+
+func (e *noPlatformColumnError) Error() string {
+	return fmt.Sprintf(`%v!A1: can't find any of %v columns in the first row`, e.tab, e.platformNames)
+}
+
+func (e *formatKeyNotSpecifiedError) Error() string {
+	return fmt.Sprintf(`%v: format name is not specified`, e.cell)
 }
 
 func (e *formatValueNotSpecifiedError) Error() string {
-	return fmt.Sprintf(`%v!%v: value for "%v" platform is not specified`, e.tab, e.row, e.platformName)
+	return fmt.Sprintf(`%v: value for "%v" platform is not specified`, e.cell, e.platformName)
+}
+
+func (e *wrongValueTypeError) Error() string {
+	return fmt.Sprintf(`%v: wrong value type`, e.cell)
+}
+
+func (e *wrongKeyTypeError) Error() string {
+	return fmt.Sprintf(`%v: wrong key type`, e.cell)
 }
