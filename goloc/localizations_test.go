1aae8fc37452834cf8f2b5d1c857da8cb2c9b16d
Alex Isaienko <thesonerik@gmail.com>
2018-04-09 03:30:18 +0300

    Added tests for localizations.go

---
+++ goloc/localizations_test.go
@@ -0,0 +1,197 @@
+package goloc
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func formats() Formats {
+	data := [][]interface{}{
+		{"format", "mock"},
+		{"x", "s"},
+		{"y", "%s"},
+		{"z", "%s"},
+	}
+
+	platform := newMockPlatform(nil)
+	formats, _ := ParseFormats(data, platform, "", "format")
+	return formats
+}
+
+func TestLocalizationsEmptyData(t *testing.T) {
+	var data [][]interface{}
+
+	p := newMockPlatform(nil)
+	_, err := ParseLocalizations(data, p, formats(), "", "", true)
+	assert.Error(t, err)
+	assert.IsType(t, &emptySheetError{}, err)
+}
+
+func TestLocalizationsEmptyFirstRow(t *testing.T) {
+	data := [][]interface{}{
+		{},
+		{"x"},
+	}
+
+	p := newMockPlatform(nil)
+	_, err := ParseLocalizations(data, p, formats(), "", "", true)
+	assert.IsType(t, &firstRowNotFoundError{}, err)
+}
+
+func TestLocalizationsNoKeyColumn(t *testing.T) {
+	data := [][]interface{}{
+		{"", "lang_en"},
+	}
+
+	p := newMockPlatform(nil)
+	_, err := ParseLocalizations(data, p, formats(), "", "key", true)
+	assert.Error(t, err)
+	assert.IsType(t, &columnNotFoundError{}, err)
+}
+
+func TestLocalizationsNoLangColumns(t *testing.T) {
+	data := [][]interface{}{
+		{"key", "something", "something else"},
+	}
+
+	p := newMockPlatform(nil)
+	_, err := ParseLocalizations(data, p, formats(), "", "key", true)
+	assert.Error(t, err)
+	assert.IsType(t, &langColumnsNotFoundError{}, err)
+}
+
+func TestLocalizationsMissingKey(t *testing.T) {
+	dataBad := [][][]interface{}{
+		{
+			{"key", "lang_en"},
+			{"", "something"},
+		},
+		{
+			{"key", "lang_en"},
+			{" ", "something"},
+		},
+		{
+			{"lang_en", "key"},
+			{"something"},
+		},
+		{
+			{"lang_en", "key"},
+			{"something", ""},
+		},
+		{
+			{"lang_en", "key"},
+			{"something", " "},
+		},
+	}
+
+	dataGood := [][][]interface{}{
+		{
+			{"key", "lang_en"},
+			{"k", "something"},
+		},
+		{
+			{"key", "lang_en"},
+			{"k", "something {x}"},
+		},
+		{
+			{"lang_en", "key"},
+			{"something", "k"},
+		},
+		{
+			{"lang_en", "key"},
+			{"something {x}", "k"},
+		},
+	}
+
+	for _, d := range dataBad {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Error(t, err)
+		assert.IsType(t, &keyMissingError{}, err)
+	}
+
+	for _, d := range dataGood {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Nil(t, err)
+	}
+}
+
+func TestLocalizationsMissingFormat(t *testing.T) {
+	dataBad := [][][]interface{}{
+		{
+			{"key", "lang_en"},
+			{"p1", "something {x}"},
+			{"p2", "something {y}"},
+			{"m", "something {missing}"},
+			{"p3", "something {z}"},
+		},
+	}
+
+	dataGood := [][][]interface{}{
+		{
+			{"key", "lang_en"},
+			{"p1", "something {x}"},
+			{"p2", "something {y}"},
+			{"p3", "something {z}"},
+		},
+	}
+
+	for _, d := range dataBad {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Error(t, err)
+		assert.IsType(t, &formatNotFoundError{}, err)
+	}
+
+	for _, d := range dataGood {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Nil(t, err)
+	}
+}
+
+func TestLocalizationsMissingLocalization(t *testing.T) {
+	dataBad := [][][]interface{}{
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"m", "something {y}"},
+		},
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"m", "something {y}", ""},
+		},
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"m", "something {y}", " "},
+		},
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"m", "", "что-то {y}"},
+		},
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"m", " ", "что-то {y}"},
+		},
+	}
+
+	dataGood := [][][]interface{}{
+		{
+			{"key", "lang_en", "lang_ru"},
+			{"p", "something {x}", "что-то {x}"},
+		},
+	}
+
+	for _, d := range dataBad {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Error(t, err)
+		assert.IsType(t, &localizationMissingError{}, err)
+	}
+
+	for _, d := range dataGood {
+		p := newMockPlatform(nil)
+		_, err := ParseLocalizations(d, p, formats(), "", "key", true)
+		assert.Nil(t, err)
+	}
+}
