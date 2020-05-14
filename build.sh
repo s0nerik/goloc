06d0972566ae2b77ff4976f937da1b988c7110aa
Alex Isaienko <thesonerik@gmail.com>
2020-05-14 21:35:04 +0300

    Added ability to use goloc from PATH.

--- build.sh
+++ build.sh
@@ -6,17 +6,28 @@ cd .tmp
 cat <<EOT >> goloc.sh
 #!/bin/bash
 
-case "\$OSTYPE" in
-  darwin*)  EXECUTABLE="darwin_amd64" ;;
-  linux*)   EXECUTABLE="linux_amd64" ;;
-  msys*)    EXECUTABLE="windows_amd64.exe" ;;
-  *)
-	  echo "Platform is not supported: \$OSTYPE"
-	  exit 1
-  ;;
-esac
+if [ -x "\$(command -v goloc)" ]; then
+  echo "Using goloc from PATH..."
+  echo "Version:"
+  goloc --version
+  goloc "\$@"
+  exit 0
+else
+  case "\$OSTYPE" in
+    darwin*)  EXECUTABLE="darwin_amd64" ;;
+    linux*)   EXECUTABLE="linux_amd64" ;;
+    msys*)    EXECUTABLE="windows_amd64.exe" ;;
+    *)
+      echo "Platform is not supported: \$OSTYPE"
+      exit 1
+    ;;
+  esac
 
-\${0%/*}/\${EXECUTABLE} "\$@"
+  echo "Using goloc from project..."
+  echo "Version:"
+  "\${0%/*}"/\${EXECUTABLE} --version
+  "\${0%/*}"/\${EXECUTABLE} "\$@"
+fi
 EOT
 
 zip -r goloc.zip .
